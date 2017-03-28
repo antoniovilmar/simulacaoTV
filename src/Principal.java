f95f5c4edbf67231c38bcd7ae293c0d785abed62
Antônio Vilmar Silveira Castro <antonios@dbserver.com.br>
2017-03-27 21:41:29 -0300

    Criação  do projeto com as classes principais

---
+++ src/Principal.java
@@ -0,0 +1,27 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		TV tvDoQuarto = new TV("Sony", 12, 20, true);
+		System.out.println("Modelo: " + tvDoQuarto.getModelo());
+		System.out.println("Canal: " + tvDoQuarto.getCanal());
+		System.out.println("Volume: " + tvDoQuarto.getVolume());
+
+		tvDoQuarto.aumentarCanal();
+		System.out.println("Canal foi aumentado para: " + tvDoQuarto.getCanal());
+		tvDoQuarto.diminuirCanal();
+		System.out.println("Canal foi diminuido para: " + tvDoQuarto.getCanal());
+		tvDoQuarto.aumentarVolume();
+		System.out.println("Volume foi aumentado para: " + tvDoQuarto.getVolume());
+		tvDoQuarto.diminuirVolume();
+		System.out.println("Volume foi diminuido para: " + tvDoQuarto.getVolume());
+		tvDoQuarto.desligar();
+		System.out.println("Tv está ligada? " + tvDoQuarto.isLigada());
+		tvDoQuarto.ligar();
+		System.out.println("Tv está ligada? " + tvDoQuarto.isLigada());
+		tvDoQuarto.setModelo("Samsung");
+		System.out.println("Modelo alterado para " + tvDoQuarto.getModelo());
+
+	}
+
+}
---
+++ src/TV.java
@@ -0,0 +1,60 @@
+
+public class TV {
+
+	private String modelo;
+	private int canal;
+	private int volume;
+	private boolean ligada;
+	
+	public TV(String modelo, int canal, int volume, boolean ligada) {
+		this.modelo = modelo;
+		this.canal = canal;
+		this.volume = volume;
+		this.ligada = ligada;
+	}
+
+	public void aumentarVolume() {
+		this.volume = this.volume + 1;
+	}
+
+	public void diminuirVolume() {
+		this.volume = this.volume - 1;
+	}
+
+	public void aumentarCanal() {
+		this.canal = this.canal + 1;
+	}
+
+	public void diminuirCanal() {
+		this.canal = this.canal - 1;
+	}
+
+	public void ligar() {
+		this.ligada = true;
+	}
+
+	public void desligar() {
+		this.ligada = false;
+	}
+
+	public String getModelo() {
+		return this.modelo;
+	}
+
+	public boolean isLigada() {
+		return ligada;
+	}
+
+	public int getCanal() {
+		return this.canal;
+	}
+
+	public int getVolume() {
+		return this.volume;
+	}
+	
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+}
